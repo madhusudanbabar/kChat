62326ff3cf7005bcfc7ed0b6a78fce0092d3e7c9
madhusudan babar <madhusudanbabar@gmail.com>
2020-04-26 13:39:33 +0530

    display posts component

---
+++ src/app/posts/display/display.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'k-display',
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.scss']
+})
+export class DisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  posts = [
+    { title: "first post", content: "this is the content"},
+    { title: "second post", content: "this is the content"},
+    { title: "third post", content: "this is the content"}
+  ]
+
+}
